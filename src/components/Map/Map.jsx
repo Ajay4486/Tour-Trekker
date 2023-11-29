59461540fee7f6f636b260a9c652915a5f425ae7
Ajay Shitkar <ajshitkar282@gmail.com>
2023-11-30 00:19:09 +0530

    API

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles'
 
-const Map = () => {
+const Map = ({ setCoordinates , setBounds}) => {
 
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
@@ -22,7 +22,10 @@ const Map = () => {
           defaultZoom={15}
           margin={[50,50,50,50]}
           options={''}
-          onChange={''}
+          onChange={(e) => {
+            setCoordinates({lat : e.center.lat, lng:e.center.lng });
+            setBounds({ne : e.marginBounds.ne, sw: e.marginBounds.sw});
+          }}
           onChildClick={''} 
         >
 
